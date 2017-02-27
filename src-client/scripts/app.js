60270ecf58fcd24d7dfb14404dc9287566d7eb21
andicoaker <andicoaker@gmail.com>
2017-02-26 20:44:43 -0500

    initial files

--- src-client/scripts/app.js
+++ src-client/scripts/app.js
@@ -1,3 +1,10 @@
 import Backbone from 'backbone';
+import $ from 'jquery';
+
+import MultiListingView from "./views/multi-listing-view.js";
+import SingleListingView from "./views/single-listing-view.js";
+import formView from "./views/form-view.js";
+
+import { MegsListCollection, ListingModel} from './models/models.js'
 
 console.log('whaaat')
